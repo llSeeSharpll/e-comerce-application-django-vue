712070158cacc7f729ae8ec31a3c17631e270c5e
llSeeSharpll <79083381+llSeeSharpll@users.noreply.github.com>
2021-05-05 11:45:37 +0300

    Update settings.py

--- django_onstore/django_onstore/settings.py
+++ django_onstore/django_onstore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r9_w!cj271fetkzf+y*@e8m#e2lu*(40!j9%naa8ils(2gp8+u'
+SECRET_KEY = '#secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
